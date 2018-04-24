bcd8acd382eb916ba3ac3e6ceca45238420c6af6
Yogesh Pawar <yogeshpawar@Yogeshs-MacBook-Air.local>
2018-04-24 21:07:32 +0530

    added new test case

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -2,16 +2,31 @@ package com.mulesoft.training;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
+	
+	@Rule
+	public DynamicPort myPort = new DynamicPort("http.port");
+	
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpect("mavenFlow", "Hello Maven");
+        System.out.println("---->>>>>> test case 1 "+myPort.getNumber());
     }
-    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("retrieveFlights");
+      System.out.println("---->>>>>> test case 2 "+myPort.getNumber());
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
+    }
+
     @Override
     protected String getConfigFile() {
         return "maven-project.xml";
