9bc23dbaad99b90bce7d7ce8bd0753c9a949fbd0
Yogesh Pawar <yogeshpawar@Yogeshs-MacBook-Air.local>
2018-04-25 20:03:24 +0530

    initial commit

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -28,8 +28,9 @@ public class HelloMavenTest extends FunctionalTestCase {
     }
 
     @Override
-    protected String getConfigFile() {
-        return "maven-project.xml";
+    protected String[] getConfigFiles() {
+    	String[] files = {"maven-project.xml", "global.xml"};
+        return files;
     }
 
 }
